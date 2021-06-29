0c7d7d26d7bb97af6e72c25e6d9c66fa1c6b9632
kodiak <kjnorth@ucsc.edu>
2021-06-29 15:34:11 -0700

    sending strings to arduino for arduino to print out. need to sort out issue with variadic arguments.. basically need to learn how they work

--- Mega2560_SPISlave/src/main.cpp
+++ Mega2560_SPISlave/src/main.cpp
@@ -8,18 +8,20 @@
 #include <SPI.h>
 #include "DataLog.h"
 
-#define DUMMY_DATA 0xFF
+#define PIC32_SERIAL Serial1
+#define DUMMY_SPI_DATA 0xFF
 
 volatile uint8_t rec, send;
 
 void setup() {
   Serial.begin(115200);
+  PIC32_SERIAL.begin(115200);
   LogInfo("SPI Slave project begins\n");
 
   uint8_t sreg = SREG; // save the current status register (SREG) configuration
   // init the SPI
   rec = 0;
-  send = DUMMY_DATA;
+  send = DUMMY_SPI_DATA;
   SPDR = send;
   pinMode(SS, INPUT);
   pinMode(MOSI, INPUT);
@@ -35,10 +37,18 @@ void loop() {
     LogInfo(F("received byte: 0x%X\n"), rec);
     preRec = rec;
   }
+
+  if (PIC32_SERIAL.available()) {
+    char message[255];
+    int size = PIC32_SERIAL.readBytesUntil('\e', message, 255);
+    for (int i = 0; i < size; i++) {
+      Serial.print(message[i]);
+    }
+  }
 }
 
 ISR (SPI_STC_vect) {
   rec = SPDR;
-  send = DUMMY_DATA;
+  send = DUMMY_SPI_DATA;
   SPDR = send;
 }
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -8,12 +8,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdarg.h>
+#include <string.h>
 
 #include "peripheral/gpio/plib_gpio.h"
 #include "peripheral/coretimer/plib_coretimer.h"
 #include "peripheral/spi/spi_master/plib_spi4_master.h"
 #include "definitions.h"
 
+#define PRINT_ACK 0xAC
+
+void Print(const char* str, ...);
 void FlashLED1(void);
 
 /*
@@ -29,10 +34,26 @@ int main(int argc, char** argv) {
         FlashLED1();
         SPI4_Write(&data, 1);
         data++;
-        CORETIMER_DelayMs(100);
+        Print("Hello Arduino, %d, %u\n\e", (int)-12, (uint8_t)158);
     }
 
     return (EXIT_SUCCESS);
+}
+
+void Print(const char* str, ...) {
+    const uint8_t length = strlen(str);
+    char message[length];
+    va_list args;
+    va_start(args, str);
+    sprintf(message, str, args);
+    UART1_Write(message, length);
+    va_end(args);
+    
+    // simple test
+//    UART1_Write((char*)str, strlen(str));
+    
+    // write str, loop until ack is received, illuminate red LED if ack never received
+//    UART1_Read()
 }
 
 void FlashLED1(void) {
